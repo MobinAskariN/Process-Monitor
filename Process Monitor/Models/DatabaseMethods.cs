6897209437323cb86678e37811f75bc9a7af5f7a
MobinAskariN <160580813+MobinAskariN@users.noreply.github.com>
2025-04-09 15:49:44 +0330

    DatabaseMethods added

---
+++ Process Monitor/Models/DatabaseMethods.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Process_Monitor.Models
+{
+    public class DatabaseMethods
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DatabaseMethods(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ProcessData> GetProcessData()
+        {
+            List<ProcessData> p = _context.ProcessData.AsNoTracking().ToList();
+            return p;
+        }
+        public List<Grouping> GetGrouping()
+        {
+            List<Grouping> g = _context.Grouping.AsNoTracking().ToList();
+            return g;
+        }
+        public List<Relations> GetRelations()
+        {
+            List<Relations> r = _context.Relation.AsNoTracking().ToList();
+            return r;
+        }
+
+    }
+}
