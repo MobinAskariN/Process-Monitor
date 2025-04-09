4fc7f294b2cfa57e05666169ed73c658c2313a19
MobinAskariN <160580813+MobinAskariN@users.noreply.github.com>
2025-04-09 15:38:34 +0330

    AplicationDbContext & NuGet packages added

---
+++ Process Monitor/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace Process_Monitor.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // If you need to use a custom configuration, use the OnModelCreating method
+        // to configure entity mappings.
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // You can configure relationships or keys here if necessary
+        }
+
+        public DbSet<ProcessData> ProcessData { get; set; }
+        public DbSet<Grouping> Grouping { get; set; }
+        public DbSet<Relations> Relation { get; set; }
+    }
+}
